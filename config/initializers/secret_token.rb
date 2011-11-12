3fe0481b5d87321e56beada45e7031f011ba9cc9
Joe Goggins <joe.goggins@gmail.com>
2011-11-12 11:35:25 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShibbolethsLilHelpersApp::Application.config.secret_token = 'c0a267a80f226f330bc60461ea60eaaf2ea8ee8305d3bbab9b85898828d88de3768cebb157fb93b386ddafd309ecb4c82d8e6a9740678ea24aa8f5f30b26ee66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShibbolethsLilHelpersApp::Application.config.session_store :cookie_store, :key => '_shibboleths_lil_helpers_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShibbolethsLilHelpersApp::Application.config.session_store :active_record_store
