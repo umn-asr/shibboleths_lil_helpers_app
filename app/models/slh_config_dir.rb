6d0612011ee7a40948036476ea44fda17bf0225d
Joe Goggins <goggins@umn.edu>
2011-11-14 18:09:56 -0600

    Sweet.  This is working...ish

--- app/models/slh_config_dir.rb
+++ app/models/slh_config_dir.rb
@@ -2,9 +2,12 @@ class SlhConfigDir < ActiveRecord::Base
   belongs_to :user
   has_many :slh_strategies
 
-  before_save :generate_config_dot_rb
-  before_save :write_config_dot_rb
+  # before_save :generate_config_dot_rb
+  #before_save :write_config_dot_rb
   before_save :write_slh_describe
+  before_save :write_slh_generate
+  before_save :write_slh_verify_metadata
+  before_save :write_slh_generate_metadata
 
   TEMPLATE =<<-EOS
 <% self.slh_strategies.each do |slh_strategy| %>
@@ -30,41 +33,68 @@ end
 <% end %>
   EOS
 
-  def generate_config_dot_rb
-    self.config_dot_rb = ERB.new(TEMPLATE).result(binding)
-  end
+  # def generate_config_dot_rb
+  #   self.config_dot_rb = ERB.new(TEMPLATE).result(binding)
+  # end
 
-  def write_config_dot_rb
-    require 'fileutils'
-    da_dir = File.join(self.dir_path,'shibboleths_lil_helper')
-    FileUtils.mkdir_p da_dir
-    File.open(File.join(da_dir,'config.rb'),'w') do |f|
-      f.print self.config_dot_rb
-    end 
-  end
+  # def write_config_dot_rb
+  #   require 'fileutils'
+  #   da_dir = File.join(self.dir_path,'shibboleths_lil_helper')
+  #   FileUtils.mkdir_p da_dir
+  #   File.open(File.join(da_dir,'config.rb'),'w') do |f|
+  #     f.print self.config_dot_rb
+  #   end 
+  # end
 
   def slh_binary
     File.join(Rails.root,'lib','shibboleths_lil_helper','bin','slh')
   end
 
-  def slh_describe_command
-    "cd #{self.dir_path} && #{self.slh_binary} describe"
-  end
   def write_slh_describe
-    stdout_str = ""
-    unless
-      Open4::popen4(self.slh_describe_command) { |pid, stdin, stdout, stderr|
-        stderr_str = stderr.read
-        stdout_str = stdout.read
-        true
-      }
-       raise "COULD NOT do #{self.slh_describe_command}"
-    end
-    puts stdout_str
-    self.slh_describe = stdout_str
+    self.write_output_from_command(:slh_describe)
   end
 
-  def dir_path
-    File.join(Rails.root, 'slh_config_dirs',self.id.to_s)
+  def write_slh_generate
+    self.write_output_from_command(:slh_generate)
   end
+
+  def write_slh_verify_metadata
+    self.write_output_from_command(:slh_verify_metadata)
+  end
+
+  def write_slh_generate_metadata
+    self.write_output_from_command(:slh_generate_metadata)
+  end
+  protected
+    def command_strings(command_sym)
+      t=command_sym.to_s.gsub(/^slh_/,'')
+      "#{slh_command_prefix} #{t}"
+    end
+
+    def write_output_from_command(command_sym)
+      the_command_string = self.command_strings(command_sym)
+      stdout_str = ""
+      stderr_str = ""
+      unless
+        Open4::popen4(the_command_string) { |pid, stdin, stdout, stderr|
+          stderr_str = stderr.read
+          stdout_str = stdout.read
+          true
+        }
+         raise "COULD NOT do #{the_command_string}"
+      end
+      v = stdout_str
+      unless stderr_str.blank?
+        v += "STDERR:\n"
+        v += stderr_str
+      end
+      write_attribute(command_sym, v)
+    end
+
+    def slh_command_prefix
+      "cd #{self.dir_path} && #{self.slh_binary} "
+    end
+    def dir_path
+      File.join(Rails.root, 'slh_config_dirs',self.id.to_s)
+    end
 end
